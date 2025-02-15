c3f587c5f3c7d6bb68c731add7509e2bf00f2236
Mikkareem <mohdirsath1997@gmail.com>
2025-02-15 13:01:32 +0530

    Bug fixes

--- java-execution-service/build.gradle.kts
+++ java-execution-service/build.gradle.kts
@@ -1,12 +1,4 @@
-import org.gradle.kotlin.dsl.withType
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
 dependencies {
     implementation(project(":common"))
     implementation(project(":code-execution-domain"))
-}
-
-
-tasks.withType<BootJar> {
-    mainClass.set("com.techullurgy.codehorn.java.execution.CodeHornJavaExecutionApplication")
 }
