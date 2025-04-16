97147b50b4c847a67a795f44ece60040699a007c
Mikkareem <mohdirsath1997@gmail.com>
2025-04-16 20:26:41 +0530

    IDX integration

---
+++ .idx/dev.nix
@@ -0,0 +1,40 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.zulu17
+    pkgs.maven
+    pkgs.consul
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "vscjava.vscode-java-pack"
+      "rangav.vscode-thunder-client"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install = "mvn clean install";
+      };
+      # Runs when a workspace is (re)started
+      onStart = {
+        run-server = "PORT=3000 mvn spring-boot:run";
+        run-init-script = ''
+          chmod +x ./init.sh
+          ./init.sh
+          echo 'Done'
+        '';
+
+        consul-start = ''
+          consul agent -dev -client=0.0.0.0 -ui -bind=127.0.0.1 -http-port=8500
+        '';
+      };
+    };
+  };
+}
